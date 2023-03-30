dc2a1d6cab3684994509e0e5494a47430e2b7006
fifthsegment <abdullah.irfan1@gmail.com>
2023-03-30 14:54:03 +0200

    update

--- server/src/app.ts
+++ server/src/app.ts
@@ -87,7 +87,7 @@ app.use(bodyParser.json());
 
     app.use("/api/info", infoRouter);
 
-    app.use("/app/static", express.static("frontend/build"));
+    app.use("/app/static", express.static("./frontend/build"));
 
     app.use("/app/*", function (req, res) {
       const filePath = path.normalize(__dirname + "/..") + "/frontend/build/index.html";
