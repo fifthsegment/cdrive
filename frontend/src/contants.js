45b19dee8ddc22bc38af51b0f82d6f136d412801
fifthsegment <abdullah.irfan1@gmail.com>
2023-03-30 12:59:13 +0200

    update

--- frontend/src/contants.js
+++ frontend/src/contants.js
@@ -1,6 +1,6 @@
 // export const SERVER_BASE_URL = "http://cdrive.ubwiservice.com";
 const {NODE_ENV} = process.env;
-export const SERVER_BASE_URL = NODE_ENV==="production" ? "/" : "http://localhost:3000";
+export const SERVER_BASE_URL = NODE_ENV==="production" ? window.location.origin : "http://localhost:3000";
 
 export const ROOT_FOLDER = {
   name: "root",
