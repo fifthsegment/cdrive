a901cf2e7d98e546df8db3ec81813a9a843a71a0
fifthsegment <abdullah.irfan1@gmail.com>
2023-03-30 15:00:18 +0200

    route update

--- server/src/app.ts
+++ server/src/app.ts
@@ -89,7 +89,7 @@ app.use(bodyParser.json());
 
     app.use("/app/static", express.static("./frontend/build"));
 
-    app.use("/app/*", function (req, res) {
+    app.use(/\/app\/(?!static).*/, function (req, res) {
       const filePath = path.normalize(__dirname + "/..") + "/frontend/build/index.html";
       res.sendFile(filePath, function (err) {
         if (err) {
